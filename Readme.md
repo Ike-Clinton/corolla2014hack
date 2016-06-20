c499fce6322001e57327e9b795c25eb7a5afaafd
Ike-Clinton <ikeclinton@gmail.com>
2016-06-20 12:10:45 -0400

    working on script

--- Readme.md
+++ Readme.md
@@ -1,2 +1,2 @@
-corolla2014hack, a program designed to read, inject, and aid in reverse
+Corolla2014hack, a program designed to read, inject, and aid in reverse
 engineering CAN bus messages in a 2014 Toyota Corolla S
--- make_vcan0.sh
+++ make_vcan0.sh
@@ -2,7 +2,7 @@
 # Display usage parameters
 display_usage() {
 	echo "This script must be run as super-user."
-	echo -e "\nUsage: \n$0 [arguments] \n"
+	echo -e "\nUsage: \nsudo $0 [interface name] \n"
 }
 
 if [ $# -le 1 ]
@@ -17,10 +17,9 @@ then
 	exit 0
 fi
 #Display usage if script not run as root/sudo
-if [[$USER != "root"]]; 
-then
-	echo "This script must be run as super-user!"
-	exit 1
+if [ "$EUID" -ne 0 ]
+	then echo "This script must be run as super-user!"
+	exit
 fi
 # Begin script
 echo "All good!"
